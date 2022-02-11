8788b2ad155e1ecd35e565c22a0234136b6eb574
Anilton Francisco de Morais Junior <anilton.morais@bettatecnologia.com.br>
2022-02-10 23:39:50 -0300

    Implementando game forca

--- forca.py
+++ forca.py
@@ -3,6 +3,25 @@ def jogar():
     print("Bem vindo ao jogo da Forca !")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    chutes = ["_" for l in palavra_secreta]
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra ? ({})".format(''.join(chutes)))
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                chutes[index] = letra
+            index += 1
+        if ("_" not in chutes):
+            print('Palavra "{}" completada !!'.format(palavra_secreta))
+            break
+
     print("################### Fim do jogo! ####################")
 
 if(__name__ == "__main__"):
