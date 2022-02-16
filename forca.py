4ee3d5a836a2b6543837bc60f550816053d55131
Anilton Francisco de Morais Junior <anilton.morais@bettatecnologia.com.br>
2022-02-16 00:15:30 -0300

    Aprimorando o jogo; Trabalhando leitura de arquivos para randomicamente escolher a palavra secreta,

--- forca.py
+++ forca.py
@@ -1,26 +1,46 @@
+import re
+from random import randrange
+
 def jogar():
     print("*********************************")
     print("Bem vindo ao jogo da Forca !")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    with open('palavras.txt', 'r') as arquivo:
+        palavras = [p.strip() for p in arquivo]
+
+    palavra_secreta = palavras[randrange(0, len(palavras))]
+    palavra_secreta = palavra_secreta.upper()
+
     enforcou = False
     acertou = False
 
     chutes = ["_" for l in palavra_secreta]
 
+    erros = 6
+
     while(not enforcou and not acertou):
-        chute = input("Qual letra ? ({})".format(''.join(chutes)))
-        chute = chute.strip()
+        print("Você tem {} tentativas".format(erros))
+        chute = input("Qual letra ? {} \n".format(' '.join([l.upper() for l in chutes])))
+        chute = chute.strip().upper()
 
         index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                chutes[index] = letra
-            index += 1
+        if (chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    chutes[index] = letra
+                index += 1
+        else:
+            erros -= 1
+
         if ("_" not in chutes):
-            print('Palavra "{}" completada !!'.format(palavra_secreta))
+            print('Você ganhou !!!')
+            print('Palavra "{}" completada !!'.format(palavra_secreta.upper()))
             break
+        if (erros == 0):
+            print('Enforcou !!!')
+            break
+
 
     print("################### Fim do jogo! ####################")
 
